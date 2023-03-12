bdfcc3c044bba1cb3d31fedfa78f38862331c96b
Aritra Das <aritrarick2002@gmail.com>
2023-03-12 19:11:53 +0530

    Connected to DB

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Homepage");
+})
+
+app.use(express.json());
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
